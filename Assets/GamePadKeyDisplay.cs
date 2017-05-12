7ff1f61642363024adf20f60d0f3e339038ad2c5
ConsciousObserver <ConsciousObserver@nomail.com>
2017-05-11 23:06:33 -0400

    Added code to get the details of the keys pressed on keyboard/gamepad/mouse or the joystick

---
+++ Assets/GamePadKeyDisplay.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GamePadKeyDisplay : MonoBehaviour {
+
+    private Text countText;
+    private Text keysText;
+    private Text joyStickText;
+
+    private int count;
+    private int countAnyKey;
+
+	void Start () {
+        countText = transform.Find("CountText").GetComponent<Text>();
+        keysText = transform.Find("KeysText").GetComponent<Text>();
+        joyStickText = transform.Find("JoyStickText").GetComponent<Text>();
+    }
+
+	void Update () {
+        if (Input.GetMouseButtonDown(0) || (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began) )
+        {
+            countText.text = "Count: " + ++count;
+        }
+
+        //if(Input.anyKeyDown)
+        //{
+        //    anyKeyText.text = "AnyKey: " + ++countAnyKey;
+        //}
+
+        Array values = Enum.GetValues(typeof(KeyCode));
+
+        string keys = "";
+        foreach (KeyCode code in values)
+        {
+            if (Input.GetKey(code))
+            {
+                keys += Enum.GetName(typeof(KeyCode), code) + ", ";
+            }
+        }
+        if (keys != "")
+        {
+            Debug.Log("keys : " + keys);
+            keysText.text = "keys: " + keys;
+        }
+
+        float vertical = 0f; ;
+        float horizontal = 0f;
+
+        string joystickDirection = "";
+
+        if( (vertical = Input.GetAxis("Vertical")) != 0 || (horizontal = Input.GetAxis("Horizontal")) != 0 )
+        {
+            joystickDirection = "V=" + vertical + ", H=" + horizontal;
+        }
+
+        if (keys != "" || joystickDirection != "")
+        {
+            Debug.Log("joystickDirection : " + joystickDirection);
+            joyStickText.text = "JoyStick: " + joystickDirection;
+        }
+    }
+}
---
+++ Assets/MoveCube.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCube : MonoBehaviour {
+    public float speed = 1;
+    public bool moveWithJoystick;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float movementX;
+        float movementY;
+        if (moveWithJoystick)
+        {
+            movementX = Input.GetAxis("Horizontal");
+            movementY = Input.GetAxis("Vertical");
+        } else
+        {
+            movementX = Input.GetAxis("Mouse X");
+            movementY = Input.GetAxis("Mouse Y");
+        }
+
+        if (movementX != 0)
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * speed * movementX);
+        }
+
+        if (movementY != 0)
+        {
+            transform.Translate(Vector3.up * Time.deltaTime * speed * movementY);
+        }
+    }
+}
